410def3f50634eee5070931f1c45a6fa32571c1e
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-25 03:38:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,60 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |song|
+        song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+        song.name
+    end
+  end
+
+  def self.new_from_filename(file_name)
+    song_info = file_name.split("-")
+    song = self.find_or_create_by_name(song_info[1].gsub(".mp3", "").strip)
+    song.artist_name = song_info[0].strip
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    song_info = file_name.split("-")
+    song = self.find_or_create_by_name(song_info[1].gsub(".mp3", "").strip)
+    song.artist_name = song_info[0].strip
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 
 end
